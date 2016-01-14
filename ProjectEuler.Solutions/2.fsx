63e4257f5a6e0caaa4c3ee622cd1731c47a0cdf3
RecursiveRobot <attie.naude@live.com>
2016-01-14 11:45:31 +0200

    Implements solution to problem #2

---
+++ ProjectEuler.Solutions/2.fsx
@@ -0,0 +1,11 @@
+﻿let infiniteFibonacci = Seq.unfold (fun state ->
+    let ``n-2`` = fst state
+    let ``n-1`` = snd state
+    let n = ``n-2`` + ``n-1``
+    Some(n, (``n-1``, n))) (0,1)
+
+let answer = 
+    infiniteFibonacci
+    |> Seq.takeWhile (fun a -> a < 4000000)
+    |> Seq.filter (fun a -> a % 2 = 0) 
+    |> Seq.sum
