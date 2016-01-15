90c4724c57894fd45c499ec135c26827825b9b9f
RecursiveRobot <attie.naude@live.com>
2016-01-15 07:28:35 +0200

    Implements solution to problem #3

---
+++ ProjectEuler.Solutions/3.fsx
@@ -0,0 +1,18 @@
+﻿let getFactors a =
+    let rec getFactorsImp acc curr div =
+        match curr with
+        | 1L -> acc
+        | n -> 
+            if curr % div = 0L then
+                getFactorsImp (div::acc) (curr/div) div
+            else
+                getFactorsImp acc curr (div+1L)
+
+    getFactorsImp [] a 2L
+
+
+getFactors 600851475143L                                        // Get all the factors for the number
+|> Seq.groupBy (fun a -> a)                                     // Group them together
+|> Seq.map (fun a -> (fst a, ((snd a) |> Seq.toList).Length))   // Map the groupings to get (factor, count) tuple
+|> Seq.find (fun a -> snd a = 1)                                // Get the first prime factor
+|> fst                                                          // Return the factor itself
