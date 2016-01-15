73340e5b4a38afde58c29ba95742d26e13bb80aa
RecursiveRobot <attie.naude@live.com>
2016-01-15 08:13:46 +0200

    Implements solution to problem #4

---
+++ ProjectEuler.Solutions/4.fsx
@@ -0,0 +1,12 @@
+﻿let ``All products of two 3-digit numbers`` = seq { 
+    for x in 100 .. 999 do
+         for y in 100 .. 999 do
+           yield x * y
+}
+    
+let isPalindrome a =
+    string a = new string(Array.rev((string a).ToCharArray()))
+
+``All products of two 3-digit numbers``
+|> Seq.filter isPalindrome
+|> Seq.max
