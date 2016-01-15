129535e37615b46171e1b5547053eaf0c940ee1e
Attie Naude <attie.naude@live.com>
2016-01-15 20:03:59 +0200

    Implements solution for problem #5

--- ProjectEuler.Solutions/3.fsx
+++ ProjectEuler.Solutions/3.fsx
@@ -1,17 +1,6 @@
-﻿let getFactors a =
-    let rec getFactorsImp acc curr div =
-        match curr with
-        | 1L -> acc
-        | n -> 
-            if curr % div = 0L then
-                getFactorsImp (div::acc) (curr/div) div
-            else
-                getFactorsImp acc curr (div+1L)
+﻿#load "Common.fs"
 
-    getFactorsImp [] a 2L
-
-
-getFactors 600851475143L                                        // Get all the factors for the number
+Common.getFactors 600851475143L                                        // Get all the factors for the number
 |> Seq.groupBy (fun a -> a)                                     // Group them together
 |> Seq.map (fun a -> (fst a, ((snd a) |> Seq.toList).Length))   // Map the groupings to get (factor, count) tuple
 |> Seq.find (fun a -> snd a = 1)                                // Get the first prime factor
---
+++ ProjectEuler.Solutions/Common.fs
@@ -0,0 +1,14 @@
+﻿module Common
+
+let getFactors a =
+    let rec getFactorsImp acc curr div =
+        match curr with
+        | 1L -> acc
+        | n -> 
+            if curr % div = 0L then
+                getFactorsImp (div::acc) (curr/div) div
+            else
+                getFactorsImp acc curr (div+1L)
+
+    getFactorsImp [] a 2L
+
