926b46ef7bb8ed10db09f4aaf2f22b14c1ad40e1
RecursiveRobot <attie.naude@live.com>
2016-01-18 11:45:15 +0200

    Implements solution to problem #12

---
+++ ProjectEuler.Solutions/12.fsx
@@ -0,0 +1,17 @@
+﻿#load "Common.fs"
+
+let triangleNumbers = Seq.initInfinite id |> Seq.map (fun a -> (int64)a) |> Seq.scan (+) 0L |> Seq.skip 2
+
+let isTriangleNumber (a : int64) =
+    triangleNumbers
+    |> Seq.takeWhile (fun b -> b <= a)
+    |> Seq.exists (fun c -> c = a)
+
+let numberOfFactors' a =
+    Common.primeFactors a
+    |> Seq.groupBy id
+    |> Seq.fold (fun acc elem -> acc * (Seq.length (snd elem) + 1)) 1
+
+let bruteForceAnswer =
+    triangleNumbers
+    |> Seq.find (fun a -> numberOfFactors' a > 500)
