593bbfd5b5307265b0242631c9be6ae5e5baa948
RecursiveRobot <attie.naude@live.com>
2016-01-18 12:21:52 +0200

    Implements solution to problem #14

---
+++ ProjectEuler.Solutions/14.fsx
@@ -0,0 +1,12 @@
+﻿let collatzSequenceLength a =
+    let rec collatzSequenceLengthImp acc elem =
+        match elem with
+        | 1L -> acc
+        | n when n%2L = 0L -> collatzSequenceLengthImp (acc+1L) (n/2L)
+        | n -> collatzSequenceLengthImp (acc+1L) (3L*n + 1L)
+
+    collatzSequenceLengthImp 0L a
+
+let answer =
+    [1L..1000000L]
+    |> Seq.maxBy collatzSequenceLength
