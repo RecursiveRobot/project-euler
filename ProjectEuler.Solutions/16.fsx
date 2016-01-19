278f708a5769f3e96e9d324d101af330d2d9da6b
Attie Naude <attie.naude@live.com>
2016-01-19 19:59:39 +0200

    Implements solution to problem #16

---
+++ ProjectEuler.Solutions/16.fsx
@@ -0,0 +1,11 @@
+﻿open System
+
+let sumOfDigits n =
+    ((string)(n)).ToCharArray()
+    |> Seq.map (fun a -> Int32.Parse(a.ToString()))
+    |> Seq.fold (fun acc elem -> acc + elem) 0
+
+let answer =
+    2I**1000
+    |> sumOfDigits
+    
