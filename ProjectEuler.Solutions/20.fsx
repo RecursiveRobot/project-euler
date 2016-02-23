51a96a7e9b6f248cfed49a0b88b4d70666c68851
Attie Naude <attie.naude@live.com>
2016-02-23 21:14:29 +0200

    Implements solution to problem #20

---
+++ ProjectEuler.Solutions/20.fsx
@@ -0,0 +1,12 @@
+﻿#load "Common.fs"
+
+open Common
+open System
+
+let answer =
+    factorial 100
+    |> string
+    |> Seq.fold (fun acc elem -> 
+        match Int32.TryParse (elem |> string) with 
+           | (true, value) -> acc + value
+           | _ -> acc) 0
